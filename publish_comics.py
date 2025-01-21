c9ece0ee30e991e54dd72c78d7e5566f3a73ee4b
Igor Derevnin <shroom2207@yandex.ru>
2025-01-21 23:57:02 +0300

    Подробности в комментариях к коммиту

--- publish_comics.py
+++ publish_comics.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import telegram
 
 
-def fetch_comics_info(num=1) -> requests.Response:
+def fetch_comics_info(num: int = 1) -> requests.Response:
     link_template = f"https://xkcd.com/{num}/info.0.json"
     response = requests.get(link_template, timeout=20)
     response.raise_for_status()
@@ -24,43 +24,43 @@ def get_img_format(link: str) -> str:
 def save_comics_image(
     link, folder_name: str = "comics", img_title: str = "img_title"
 ) -> str:
-    os.makedirs(folder_name, exist_ok=True)
     img_format = get_img_format(link)
     img_name = f"{img_title}.{img_format}"
     img_path = os.path.join(folder_name, img_name)
+    response = requests.get(link, timeout=20)
+    response.raise_for_status()
     with open(img_path, "wb") as image:
-        response = requests.get(link, timeout=20)
-        response.raise_for_status()
         image.write(response.content)
     return img_path
 
 
-def get_current_comics_info() -> requests.Response:
+def get_rnd_comics_num() -> int:
     response = requests.get("https://xkcd.com/info.0.json", timeout=20)
     response.raise_for_status()
-    return response.json()
-
-
-def save_random_comics(folder_name: str = "comics") -> tuple[str, str]:
-    current_comics_num = int(get_current_comics_info()["num"])
-    random_num = random.randint(1, current_comics_num)
-    comics_info = fetch_comics_info(random_num)
-    comics_alt = comics_info["alt"]
-    img_link = comics_info["img"]
-    img_safe_title = comics_info["safe_title"]
-    comics_path = save_comics_image(img_link, folder_name, img_safe_title)
-    return comics_path, comics_alt
+    current_comics_num = int(response.json()["num"])
+    return random.randint(1, current_comics_num)
 
 
 def main():
     load_dotenv(".env")
     bot_token = os.environ["TG_BOT_TOKEN"]
     group_id = os.environ["TG_GROUP_ID"]
+    folder_name = os.getenv("FOLDER")
+    if not folder_name:
+        folder_name = "comics"
+    os.makedirs(folder_name, exist_ok=True)
     bot = telegram.Bot(bot_token)
-    path, alt = save_random_comics()
-    with open(path, "rb") as photo:
-        bot.send_photo(chat_id=group_id, photo=photo, caption=alt)
-    os.remove(path)
+    try:
+        rnd_comics_num = get_rnd_comics_num()
+        rnd_comics_info = fetch_comics_info(rnd_comics_num)
+        alt = rnd_comics_info["alt"]
+        img_link = rnd_comics_info["img"]
+        safe_title = rnd_comics_info["safe_title"]
+        img_path = save_comics_image(img_link, folder_name, safe_title)
+        with open(img_path, "rb") as photo:
+            bot.send_photo(chat_id=group_id, photo=photo, caption=alt)
+    finally:
+        os.remove(img_path)
 
 
 if __name__ == "__main__":
